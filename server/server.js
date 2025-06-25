db6321fb00c9855e27eb609ca40a900a514cdc04
Shivamprajapati844505 <sp1172389@gmail.com>
2025-06-25 17:21:55 +0530

    remove env

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(clerkMiddleware())
 app.get('/',(req,res)=>
 res.send('Server is Live'))
 
-app.use('api/inngest', serve({client: inngest, functions}))
+app.use('/api/inngest', serve({client: inngest, functions}))
 
 
 app.listen(PORT,()=>{
