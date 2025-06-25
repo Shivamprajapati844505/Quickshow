6ab76bd382bb3d3885c4e3fbb188490ec56b2b2a
Shivamprajapati844505 <sp1172389@gmail.com>
2025-06-26 01:19:42 +0530

    create a endpoint of shows and  movies

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 
 const app = express();
@@ -13,15 +14,17 @@ const PORT = 3000;
 await connectDB()
 
 
+
 //Middleware
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
 
-app.get('/',(req,res)=>
-res.send('Server is Live'))
 
+//API Routes
+app.get('/',(req,res)=>res.send('Server is Live'))
 app.use('/api/inngest', serve({client: inngest, functions}))
+app.use('/api/show', showRouter)
 
 
 app.listen(PORT,()=>{
